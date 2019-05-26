04e9d4eedcc64c60eb558b6461a7523917123303
Sandip Pingle <sandip.pingle@here.com>
2019-05-26 23:30:13 +0530

    first commit

---
+++ src/tgp-spec.js
@@ -0,0 +1,9 @@
+'use strict'
+
+/* eslint-env mocha */
+const tgp = require('.')
+describe('tgp', () => {
+  it('write this test', () => {
+    console.assert(tgp, 'should export something')
+  })
+})
